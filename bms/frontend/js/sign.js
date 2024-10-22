d7e9fca5136a667fc1f7c9fd1d29a4d0e3e6e6b5
MuhammadNashid <muhammadnashid905@gmail.com>
2024-10-22 13:29:56 +0530

    updated

---
+++ bms/frontend/js/sign.js
@@ -0,0 +1,26 @@
+document.getElementById('form').addEventListener('submit',async function (e){
+    e.preventDefault();
+
+    name=document.getElementById('username').value
+    email=document.getElementById('email').value
+    pass=document.getElementById('pass').value
+    cpass=document.getElementById('cpass').value
+
+    console.log(name,email,pass,cpass);
+
+    const res=await fetch('http://localhost:4000/api/adduser',{
+        method: "post",
+        headers:{"Content-Text":"application/json"},
+        body:JSON.stringify({name,email,pass,cpass})
+    })
+    console.log(res);
+
+    const data=await res.json()
+    if(res.status==201){
+        alert(data.msg)
+        window.location.href="../index.html"
+    }
+    else{
+        alert(data.error)
+    }
+})
